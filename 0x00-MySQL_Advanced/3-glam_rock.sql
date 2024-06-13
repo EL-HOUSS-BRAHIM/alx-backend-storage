e92cb8e57ebdb5fdcd1a3f93470b0899932c64e3
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-13 23:17:33 +0100

    fix 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,18 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- Use the appropriate database
+USE holberton;
+
+-- Select bands with Glam rock as their main style, calculating lifespan and ordering by it
+SELECT 
+    name AS band_name,
+    CASE 
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    main_style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
