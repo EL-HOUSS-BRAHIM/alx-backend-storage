d20b34f5415c64020cea8d3343fe23712307dd84
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-13 23:22:26 +0100

    fix 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,9 @@
 -- Use the appropriate database
 -- Select bands with Glam rock as their main style, calculating lifespan and ordering by it
-
-
-SELECT band_name, 
-    (YEAR('2022-01-01') - SUBSTRING_INDEX(lifespan, '-', 1)) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+SELECT
+    band_name,
+    YEAR(MAX(year_end)) - YEAR(MIN(year_start)) AS lifespan
+FROM bands
+WHERE style = 'Glam rock'
+GROUP BY band_name
 ORDER BY lifespan DESC;
