064eff58f21b7f7d94eac25b9b945d3c0a2203c2
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-19 22:48:36 +0100

    web code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+""" web module """
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 # Initialize Redis client
 redis_client = redis.Redis()
-""" web module """
 
 
 def count_requests(method: Callable) -> Callable:
